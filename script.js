5781b9558574f973a1d2facbfa052b01559bc390
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-20 19:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+const itemNameInput = document.getElementById('item-name-input');
+const itemPriceInput = document.getElementById('item-price-input');
+const addButton = document.getElementById('add');
+const cart = document.getElementById('cart');
+const total = document.getElementById('total');
+
+let grandTotal = 0;
+
+function addItem() {
+    const itemName = itemNameInput.value.trim();
+    const itemPrice = parseFloat(itemPriceInput.value.trim());
+
+    if (!itemName || isNaN(itemPrice) || itemPrice <= 0) {
+        alert('Invalid input!');
+        return;
+    }
+
+    grandTotal += itemPrice;
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${itemName}</td>
+        <td>$${itemPrice.toFixed(2)}</td>
+    `;
+    cart.appendChild(newRow);
+    itemNameInput.value = '';
+    itemPriceInput.value = '';
+    total.textContent = `Grand Total: $${grandTotal.toFixed(2)}`;
+}
+
+addButton.addEventListener('click', addItem);
